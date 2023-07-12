0dd50fc105cc9749625172969a864e054f57f00e
Alec M <alec.mattu@nih.gov>
2023-07-12 09:56:22 -0400

    CRDCDH-54 Initial commit for ListView component

---
+++ src/graphql/listApplications.ts
@@ -0,0 +1,30 @@
+import gql from 'graphql-tag';
+
+export const query = gql`
+  query listApplications($first: Int, $offset: Int, $orderBy: String, $sortDirection: String) {
+    listApplications(first: $first, offset: $offset, orderBy: $orderBy, sortDirection: $sortDirection) {
+        _id
+        program {
+          abbreviation
+        }
+        study {
+          abbreviation
+        }
+        status
+        createdAt
+        updatedAt
+        submittedDate
+        applicant {
+          applicantName
+        }
+        organization
+    }
+  }
+`;
+
+export type Response = {
+  listApplications: {
+    total: number;
+    applications: RecursivePartial<Application>[];
+  };
+};
