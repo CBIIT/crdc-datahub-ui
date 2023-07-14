d61b485638f33cfbe8a831c03bc37de28813f53d
Joon Lee <joon.lee2@nih.gov>
2023-07-14 12:59:07 -0400

    Updated login to use changed env var name

--- src/content/login/Controller.tsx
+++ src/content/login/Controller.tsx
@@ -4,7 +4,7 @@
  * @returns null
  */
 const loginController = () => {
-  const NIH_AUTHENTICATION_URL = process.env.NIH_AUTHENTICATION_URL || process.env.REACT_APP_NIH_AUTHENTICATION_URL;
+  const NIH_AUTHORIZE_URL = process.env.NIH_AUTHORIZE_URL || process.env.REACT_APP_NIH_AUTHORIZE_URL;
   const NIH_CLIENT_ID = process.env.NIH_CLIENT_ID || process.env.REACT_APP_NIH_CLIENT_ID;
   const NIH_REDIRECT_URL = process.env.NIH_REDIRECT_URL || process.env.REACT_APP_NIH_REDIRECT_URL;
 
@@ -16,7 +16,7 @@ const loginController = () => {
   };
 
   const params = new URLSearchParams(urlParam).toString();
-  const redirectUrl = `${NIH_AUTHENTICATION_URL}?${params}`;
+  const redirectUrl = `${NIH_AUTHORIZE_URL}?${params}`;
   window.location.href = redirectUrl;
 
   return null;
