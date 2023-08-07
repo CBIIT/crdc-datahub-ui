f9fb56ac3de2b522850925e0e540f0100f820d73
Michael Tang <mtangmt@gmail.com>
2023-08-07 15:09:54 -0400

    adding timed out dialog

--- src/components/InactivityDialog/InactivityDialog.tsx
+++ src/components/InactivityDialog/InactivityDialog.tsx
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../Contexts/AuthContext';
 import { PING_INTERVAL, REDIRECT_AFTER_SIGN_OUT, SHOW_WARNING_BEFORE } from '../../config/siteWideConfig';
 
-const StyledDialog = styled(Dialog)`
+const InacivityWarningDialog = styled(Dialog)`
   .MuiDialog-paper {
     width: 550px;
     height: 320px;
@@ -28,7 +28,8 @@ const StyledDialog = styled(Dialog)`
     letter-spacing: 0;
   }
 `;
-const StyledDialogContent = styled.div`
+
+const InacivityWarningContent = styled.div`
     margin: 50px auto;
     color: #000000;
     font-family: Nunito;
@@ -57,11 +58,65 @@ const StyledDialogContent = styled.div`
     }
 `;
 
+const SessionTimeoutDialog = styled(Dialog)`
+  .MuiDialog-paper {
+    width: 550px;
+    height: 320px;
+    border-radius: 8px;
+    border: 2px solid var(--secondary-one, #0B7F99);
+    background: linear-gradient(0deg, #F2F6FA 0%, #F2F6FA 100%), #2E4D7B;
+    box-shadow: 0px 4px 45px 0px rgba(0, 0, 0, 0.40);
+  }
+  .closeIcon {
+    cursor: pointer;
+    text-align: end;
+  }
+`;
+
+const SessionTimeoutContent = styled.div`
+  justify-content: space-between;
+  padding-right: 33px;
+  padding-left: 33px;
+  padding-top: 10px;
+  font-family: lato;
+  text-align: center;
+  .sessionTimeoutTitle {
+    font-size: 25px;
+    font-weight: bold;
+    padding-bottom: 12px;
+    color: #566672;
+  }
+  .sessionTimeoutMessage {
+    font-size: 17px;
+    padding-bottom: 14px;
+  }
+  .alignCenter {
+      text-align: center;
+    }
+    .buttonGroup {
+      color: #FFFFFF;
+      font-family: Lato;
+      font-size: 11px;
+      line-height: 22px;
+      width: 90px;
+      border: 1px solid #626262;
+      margin-top: 30px;
+    }
+    .closeButton {
+      background-color: #566672 !important;
+    }
+    .loginButton{
+      margin-left: 20px;
+      background-color: #437BBE !important;
+    }
+`;
+
 const InactivityDialog = () => {
   const authData = useAuthContext();
   const { isLoggedIn } = authData;
-  const [open, setOpen] = useState(true);
-
+  const [warning, setWarning] = useState(false);
+  const [timedOut, setTimedOut] = useState(true);
+  const navigate = useNavigate();
   const extendSession = () => {
     // todo: actual extend session logic
     console.log("session extended");
@@ -69,41 +124,82 @@ const InactivityDialog = () => {
 
   const handleExtendSession = () => {
     extendSession();
-    setOpen(false);
+    setWarning(false);
   };
   const handleSignOut = () => {
     authData.logout();
-    setOpen(false);
+    setWarning(false);
   };
 
   return (
-    <StyledDialog open={open}>
-      <DialogTitle id="customized-dialog-title"> Session Timeout Warning</DialogTitle>
-      <StyledDialogContent>
-        This session is about to expire due to inactivity.
-        <br />
-        You will be logged out in 3 minutes.
-        <br />
-        Please elect to extend this session or logout.
-        <div className="alignCenter">
-          <Button
-            variant="contained"
-            className="buttonGroup extendButton"
-            autoFocus
-            onClick={handleExtendSession}
+    <>
+      <InacivityWarningDialog open={warning}>
+        <DialogTitle id="customized-dialog-title"> Session Timeout Warning</DialogTitle>
+        <InacivityWarningContent>
+          This session is about to expire due to inactivity.
+          <br />
+          You will be logged out in 3 minutes.
+          <br />
+          Please elect to extend this session or logout.
+          <div className="alignCenter">
+            <Button
+              variant="contained"
+              className="buttonGroup extendButton"
+              autoFocus
+              onClick={handleExtendSession}
+            >
+              EXTEND SESSION
+            </Button>
+            <Button
+              variant="contained"
+              className="buttonGroup logOutButton"
+              onClick={handleSignOut}
+            >
+              LOGOUT
+            </Button>
+          </div>
+        </InacivityWarningContent>
+      </InacivityWarningDialog>
+      <SessionTimeoutDialog open={timedOut}>
+        <DialogTitle>
+          <div
+            role="button"
+            className="closeIcon"
+            onClick={() => setTimedOut(false)}
+            tabIndex={0}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setTimedOut(false);
+              }
+            }}
           >
-            EXTEND SESSION
-          </Button>
-          <Button
-            variant="contained"
-            className="buttonGroup logOutButton"
-            onClick={handleSignOut}
-          >
-            LOGOUT
-          </Button>
-        </div>
-      </StyledDialogContent>
-    </StyledDialog>
+            <img style={{ height: 10, marginBottom: 2 }} src="https://raw.githubusercontent.com/CBIIT/datacommons-assets/main/bento/images/icons/svgs/LocalFindCaseDeleteIcon.svg" alt="close icon" />
+          </div>
+        </DialogTitle>
+        <SessionTimeoutContent>
+          <div className="sessionTimeoutTitle">Your session has expired.</div>
+          <br />
+          <div className="sessionTimeoutMessage">Please login again to continue working.</div>
+          <div className="alignCenter">
+            <Button
+              variant="contained"
+              className="buttonGroup closeButton"
+              autoFocus
+              onClick={() => setTimedOut(false)}
+            >
+              CLOSE
+            </Button>
+            <Button
+              variant="contained"
+              className="buttonGroup loginButton"
+              onClick={() => { setTimedOut(false); navigate("login"); }}
+            >
+              LOGIN
+            </Button>
+          </div>
+        </SessionTimeoutContent>
+      </SessionTimeoutDialog>
+    </>
   );
 };
 
