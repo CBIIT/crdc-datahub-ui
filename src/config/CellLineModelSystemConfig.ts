79402472ee6ca2f3721c59a124de75873d82f4d8
Alec M <alec.mattu@nih.gov>
2023-08-07 14:17:56 -0400

    Validate GroupCheckbox and Radio

--- src/config/CellLineModelSystemConfig.ts
+++ src/config/CellLineModelSystemConfig.ts
@@ -1,20 +1,18 @@
-import { FormGroupCheckboxOption } from "../components/Questionnaire/FormGroupCheckbox";
+/**
+ * Configuration for Questionnaire Section C Cell Line Model System
+ *
+ */
+const options: FormGroupCheckboxOption[] = [
+  {
+    label: "Cell lines",
+    value: "Cell lines",
+    name: "cellLines",
+  },
+  {
+    label: "Model systems",
+    value: "Model systems",
+    name: "modelSystems",
+  },
+];
 
-  /**
-   * Configuration for Questionnaire Section C Cell Line Model System
-   *
-   */
-  const options: FormGroupCheckboxOption[] = [
-    {
-      label: "Cell lines",
-      value: "Cell lines",
-      name: "cellLines",
-    },
-    {
-      label: "Model systems",
-      value: "Model systems",
-      name: "modelSystems",
-    },
-  ];
-
-  export default options;
+export default options;
