34eb51f9c244297f95605b0cd68da9110c8d4745
Michael Tang <mtangmt@gmail.com>
2023-08-15 11:37:06 -0400

    actually should be CRDC DataHub

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -19,7 +19,7 @@ interface LayoutProps {
 const Layout: FC<LayoutProps> = ({ children }) => (
   <>
     <Helmet>
-      <title>CRDC Hub</title>
+      <title>CRDC DataHub</title>
       <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
       {/* List of fonts here:
         <link href="https://fonts.googleapis.com/css2?
