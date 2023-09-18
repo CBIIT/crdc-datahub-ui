f9b5bc1b1b0fa944ef08c81f31b47c0a2723c925
Chen <jonkiky@gmail.com>
2023-09-18 14:49:57 -0400

    removed unnecessary content

--- public/injectEnv.js
+++ public/injectEnv.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 window.injectedEnv = {
-  NIH_AUTHORIZE_URL: 'https://stsstg.nih.gov/auth/oauth/v2/authorize',
-  NIH_CLIENT_ID: 'cd99ec1a-dc26-4fd0-a2f6-9967a39d6ad6',
-  NIH_REDIRECT_URL: 'http://localhost:4010',
+  NIH_AUTHORIZE_URL: 'https://link.to/nihlogin',
+  NIH_CLIENT_ID: 'Sample Id',
+  NIH_REDIRECT_URL: 'http://link.back/toHost',
 };
