432ab10fc5e40518e87a75d046c9daffaae9115d
Alec M <alec.mattu@nih.gov>
2023-09-25 10:18:54 -0400

    CRDCDH-321 Fix error message and remove org header

--- src/types/Auth.d.ts
+++ src/types/Auth.d.ts
@@ -72,7 +72,18 @@ type EditOrganizationInput = {
 type ApprovedStudy = {
   _id: string;
   originalOrg: Organization["_id"];
+  /**
+   * Study name
+   *
+   * @example Genomic Information System
+   */
   studyName: string;
+  /**
+   * Study Abbreviation
+   * This is a unique constraint across all studies
+   *
+   * @example GIS
+   */
   studyAbbreviation: string;
   dbGapID: string;
 };
