277c81f25358406490da37ef52fb8f516523ce20
Michael Tang <mtangmt@gmail.com>
2023-10-02 11:59:03 -0400

    update header logo to closer match CRDC

--- src/components/Header/components/LogoDesktop.tsx
+++ src/components/Header/components/LogoDesktop.tsx
@@ -8,10 +8,12 @@ const LogoArea = styled.div`
     img {
       width: fit-content;
       height: 56px;
+      max-width: 100%
     }
 
     .logoContainer {
       margin-top: 35px;
+      max-width: 440px;
     }
 
 `;
