599c5187a093493e6abe32cd04bc88de12fcae27
Michael Tang <mtangmt@gmail.com>
2023-10-12 12:32:03 -0400

    DataSubmission -> submission because of backend

--- src/types/Submissions.d.ts
+++ src/types/Submissions.d.ts
@@ -1,20 +1,21 @@
 type Submission = {
-    _id: string; // aka. submissionID
-    name: string;
-    submitterID: string;
-    submitterName: string; // <first name> <last name>
-    organization: Pick<Organization, "_id", | "name">;
-    dataCommons: string;
-    modelVersion: string; // # for future use
-    studyAbbreviation: string;
-    dbGaPID: string; // # aka. phs number
-    bucketName: string; // # populated from organization
-    rootPath: string; // # a submission folder will be created under this path, default is / or "" meaning root folder
-    status: DataSubmissionStatus; // [New, In Progress, Submitted, Released, Canceled, Transferred, Completed, Archived]
-    history: [DataSubmissionHistoryEvent]
-    concierge: string; // # Concierge name
-    createdAt: string; // # ISO 8601 date time format with UTC or offset e.g., 2023-05-01T09:23:30Z
-    updatedAt: string; // # ISO 8601 date time format with UTC or offset e.g., 2023-05-01T09:23:30Z
+  _id
+  name
+  submitterID
+  submitterName
+  organization
+  dataCommons: string;
+  modelVersion: string; // # for future use
+  studyAbbreviation: string;
+  dbGaPID: string; // # aka. phs number
+  bucketName: string; // # populated from organization
+  rootPath: string; // # a submission folder will be created under this path, default is / or "" meaning root folder
+  status: DataSubmissionStatus; // [New, In Progress, Submitted, Released, Canceled, Transferred, Completed, Archived]
+  history: DataSubmissionHistoryEvent[]
+  concierge: string; // # Concierge name
+  conciergeEmail: string; // # Concierge email (MIGHT CHANGE)
+  createdAt: string; // # ISO 8601 date time format with UTC or offset e.g., 2023-05-01T09:23:30Z
+  updatedAt: string; // # ISO 8601 date time format with UTC or offset e.g., 2023-05-01T09:23:30Z
 };
 
 type SubmissionHistoryEvent = {
@@ -24,7 +25,7 @@ type SubmissionHistoryEvent = {
   userID: string;
 };
 
-type SubmissionStatus = "New" | "In Progress" | "Submitted" | "Released" | "Canceled" | "Transferred" | "Completed" | "Archived";
+type SubmissionStatus = "New" | "In Progress" | "Submitted" | "Released" | "Withdrawn" | "Rejected" | "Completed" | "Archived" | "Canceled";
 
 type HistoryEvent = {
   status: ApplicationStatus;
@@ -38,3 +39,14 @@ type Applicant = {
   applicantName: string;
   applicantEmail: string;
 };
+
+type BatchFile = {
+  _id: string;
+  uploadType: string;
+  fileCount: number;
+  status: string;
+  submittedDate: string;
+  errorCount: number;
+};
+
+type DataSubmissionHistoryEvent = HistoryBase<SubmissionStatus>;
