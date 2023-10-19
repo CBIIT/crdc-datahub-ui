582221dd754aae48939a99bdc3a7f39b5a156d81
Alejandro-Vega <vegaalejandro15@gmail.com>
2023-10-19 14:43:30 -0400

    Fixed comment

--- src/types/Submissions.d.ts
+++ src/types/Submissions.d.ts
@@ -13,7 +13,7 @@ type Submission = {
   status: SubmissionStatus; // [New, In Progress, Submitted, Released, Canceled, Transferred, Completed, Archived]
   history: SubmissionHistoryEvent[];
   conciergeName: string; // Concierge name
-  conciergeEmail: string; // Concierge name
+  conciergeEmail: string; // Concierge email
   createdAt: string; // ISO 8601 date time format with UTC or offset e.g., 2023-05-01T09:23:30Z
   updatedAt: string; // ISO 8601 date time format with UTC or offset e.g., 2023-05-01T09:23:30Z
 };
