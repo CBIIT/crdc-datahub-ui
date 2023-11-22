d98b43b019bcd61d8ac2f0cd577626bf59ba5351
Michael Tang <mtangmt@gmail.com>
2023-11-22 14:40:24 -0500

    add documentation in comment

--- src/config/globalHeaderData.tsx
+++ src/config/globalHeaderData.tsx
@@ -51,6 +51,8 @@ export const navbarSublists = {
   //     className: 'navMobileSubTitle',
   //   },
   // ],
+  //
+  // To make it a link, the className has to be navMobileSubItem
   "Model Navigator": DataCommons.map((dc) => ({
     name: `${dc.name} Model`,
     link: `/model-navigator/${dc.name}`,
