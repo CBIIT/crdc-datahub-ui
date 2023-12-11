326ef2ec622d8c47897a11409604d77730846668
Alec M <alec.mattu@nih.gov>
2023-12-11 14:05:23 -0500

    Minor styling adjustments

--- src/components/NodeTotalChart/ActiveArc.tsx
+++ src/components/NodeTotalChart/ActiveArc.tsx
@@ -1,4 +1,9 @@
-import { Sector } from 'recharts';
+import { CSSProperties } from 'react';
+import { Sector } from "recharts";
+
+const style: CSSProperties = {
+  filter: "drop-shadow(0px 0px 4px rgba(0, 0, 0, 0.3))",
+};
 
 /**
  * Renders the current active arc (cell) of the pie chart.
@@ -6,6 +11,8 @@ import { Sector } from 'recharts';
  * @param {ActiveShapeProps<PieSectorDataItem>} props
  * @returns {ActiveShape<PieSectorDataItem>}
  */
-const ActiveArc = ({ innerRadius, ...props }) => (<Sector {...props} innerRadius={innerRadius - 15} />);
+const ActiveArc = ({ innerRadius, ...props }) => (
+  <Sector {...props} innerRadius={innerRadius - 15} style={style} />
+);
 
 export default ActiveArc;
