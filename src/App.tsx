521ff6b3a0a438beb2d6d3dfd8fef8461987c128
Alejandro-Vega <vegaalejandro15@gmail.com>
2024-01-24 17:29:40 -0500

    Added styleOverrides to button theme

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,104 @@ const theme = createTheme({
       xl: 1440,
     }
   },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        disableElevation: true,
+      },
+      styleOverrides: {
+        contained: {
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "#FFF",
+          borderRadius: "8px",
+          textTransform: "initial",
+          boxShadow: "none",
+          textAlign: "center",
+          zIndex: 3,
+          "&.Mui-disabled": {
+            background: "#B1B1B1",
+            color: "#EDEDED",
+            fontWeight: 700,
+            border: "1.5px solid #6B7294",
+            "&.MuiButton-containedInfo": {
+              color: "#EDEDED",
+              fontWeight: 500
+            },
+            "& .MuiButton-startIcon, & .MuiButton-endIcon": {
+              color: "#EDEDED",
+            }
+          },
+          "&.MuiButton-containedInfo": {
+            color: "#000"
+          },
+          "& .MuiButton-startIcon": {
+            position: "absolute",
+            left: "11px",
+            color: "#6B7294"
+          },
+          "& .MuiButton-endIcon": {
+            position: "absolute",
+            right: "11px",
+            color: "#6B7294"
+          }
+        },
+        containedPrimary: {
+          border: "1.5px solid #08596C",
+          fontWeight: 700,
+          "&:hover": {
+            border: "1.5px solid #08596C",
+            background: "#1A8199",
+            backgroundImage: "linear-gradient(rgb(0 0 0/15%) 0 0)",
+          },
+        },
+        containedSuccess: {
+          border: "1.5px solid #0A6A52",
+          fontWeight: 700,
+          "&:hover": {
+            border: "1.5px solid #0A6A52",
+            background: "#1B8369",
+            backgroundImage: "linear-gradient(rgb(0 0 0/15%) 0 0)",
+          },
+        },
+        containedError: {
+          border: "1.5px solid #6C2110",
+          fontWeight: 700,
+          "&:hover": {
+            border: "1.5px solid #6C2110",
+            background: "#B34C36",
+            backgroundImage: "linear-gradient(rgb(0 0 0/15%) 0 0)",
+          },
+        },
+        containedInfo: {
+          border: "1.5px solid #6B7294",
+          fontWeight: 500,
+          "&:hover": {
+            border: "1.5px solid #6B7294",
+            // background: "#C0DAF3",
+          },
+        },
+      }
+    }
+  },
   palette: {
+    primary: {
+      main: "#1A8199",
+      contrastText: "#FFF",
+    },
+    success: {
+      main: "#1B8369",
+      contrastText: "#FFF"
+    },
+    error: {
+      main: "#B34C36",
+      contrastText: "#FFF"
+    },
+    info: {
+      main: "rgba(0,0,0,0)",
+      contrastText: "#000"
+    },
     Rejected: {
       main: "#E25C22",
       contrastText: "#FFDBCB",
