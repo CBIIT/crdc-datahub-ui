760b65d1352f2965b5f5c096f72104c11e352bfd
Alejandro-Vega <vegaalejandro15@gmail.com>
2024-05-07 12:00:07 -0400

    Created delete button component for individual orphan files

---
+++ src/components/DataSubmissions/DeleteOrphanFileButton.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { Chip, ChipProps, styled } from "@mui/material";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { useSnackbar } from "notistack";
+import { useMutation } from "@apollo/client";
+import { DELETE_EXTRA_FILE, DeleteExtraFileResp } from "../../graphql";
+
+const StyledChip = styled(Chip)({
+  "&.MuiChip-root": {
+    border: "1px solid #2B528B",
+    background: "#2B528B",
+    height: "21px",
+    marginLeft: "60.5px",
+    alignSelf: "center",
+  },
+  "& .MuiSvgIcon-root": {
+    color: "#FFFFFF",
+    fontSize: "15px",
+    marginLeft: "3.65px",
+  },
+  "& .MuiChip-label": {
+    color: "#D8E3F2",
+    fontFamily: "'Inter', 'Rubik', sans-serif",
+    fontWeight: 400,
+    fontSize: "10px",
+    lineHeight: "11px",
+    paddingLeft: "8px",
+    paddingRight: "5px",
+  },
+});
+
+type Props = {
+  submissionId: Submission["_id"];
+  submittedId: QCResult["submittedID"];
+} & ChipProps;
+
+const DeleteOrphanFileButton = ({ submissionId, submittedId, disabled, ...rest }: Props) => {
+  const { enqueueSnackbar } = useSnackbar();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [deleteExtraFile] = useMutation<DeleteExtraFileResp>(DELETE_EXTRA_FILE, {
+    context: { clientName: "backend" },
+    fetchPolicy: "no-cache",
+  });
+
+  const deleteOrphanFile = async () => {
+    setLoading(true);
+
+    const { data: d, errors } = await deleteExtraFile({
+      variables: {
+        _id: submissionId,
+        fileName: submittedId,
+      },
+    }).catch((e) => ({ errors: e?.message, data: null }));
+    setLoading(false);
+
+    if (errors || !d?.deleteAllExtraFiles?.success) {
+      enqueueSnackbar("There was an issue deleting orphan file.", {
+        variant: "error",
+      });
+      setLoading(false);
+      // return;
+    }
+
+    // console.log(d.deleteAllExtraFiles);
+  };
+
+  return (
+    <StyledChip
+      icon={<CancelIcon />}
+      label="Delete orphaned file"
+      onClick={deleteOrphanFile}
+      disabled={loading || disabled}
+      {...rest}
+    />
+  );
+};
+
+export default DeleteOrphanFileButton;
