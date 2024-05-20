6fe092ea079ed85d3e153c0342460566b6f9a22c
Alec M <alec.mattu@nih.gov>
2024-05-20 10:35:48 -0400

    fix: 404 page does not conform to brand styling

--- src/content/status/Page404.test.tsx
+++ src/content/status/Page404.test.tsx
@@ -1,10 +1,63 @@
 import { axe } from "jest-axe";
-import { render } from "@testing-library/react";
+import { BrowserRouter, RouterProvider, createMemoryRouter } from "react-router-dom";
+import { render, waitFor } from "@testing-library/react";
+import { FC } from "react";
+import userEvent from "@testing-library/user-event";
 import Page from "./Page404";
 
-it("should not have any accessibility violations", async () => {
-  const { container } = render(<Page />);
-  const results = await axe(container);
+const TestParent: FC = () => (
+  <BrowserRouter>
+    <Page />
+  </BrowserRouter>
+);
 
-  expect(results).toHaveNoViolations();
+describe("404 Page Cases", () => {
+  it("should not have any accessibility violations", async () => {
+    const { container } = render(<TestParent />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+
+  it("should render statically and correctly", () => {
+    const { container } = render(<TestParent />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should set the page title to 'Page Not Found'", async () => {
+    render(<TestParent />);
+
+    await waitFor(() => expect(document.title).toBe("Page Not Found"));
+  });
+
+  it("should navigate home when the 'Return Home' button is clicked", async () => {
+    const router = createMemoryRouter(
+      [
+        {
+          path: "/404",
+          element: <Page />,
+        },
+        {
+          path: "/",
+          element: <div>home page</div>,
+        },
+      ],
+      {
+        initialEntries: ["/404"],
+      }
+    );
+
+    const { getByText, getByRole } = render(<RouterProvider router={router} />);
+
+    await waitFor(() => {
+      expect(getByText(/page not found/i)).toBeInTheDocument();
+    });
+
+    userEvent.click(getByRole("button"));
+
+    await waitFor(() => {
+      expect(getByText(/home page/i)).toBeInTheDocument();
+    });
+  });
 });
--- src/content/status/Page404.tsx
+++ src/content/status/Page404.tsx
@@ -1,14 +1,75 @@
 import { FC } from "react";
+import { Box, Button, Stack, Typography, styled } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import usePageTitle from "../../hooks/usePageTitle";
+import status404 from "../../assets/icons/404.png";
+
+const StyledContainer = styled(Stack)({
+  background: "#114454CC",
+  padding: "69px 0",
+  alignItems: "center",
+  justifyContent: "center",
+  // NOTE: 209px is the header height. This is only visible on larger screens.
+  minHeight: "calc(100vh - 209px)",
+});
+
+const StyledContent = styled(Box)({
+  textAlign: "center",
+});
+
+const StyledImage = styled("img")({
+  width: "380.39px",
+  height: "auto",
+});
+
+const StyledHeader = styled(Typography)({
+  color: "#FFFFFF",
+  fontFamily: "Nunito",
+  fontSize: "26px",
+  fontWeight: 700,
+  lineHeight: "35px",
+});
+
+const StyledBody = styled(Typography)({
+  marginTop: "14px",
+  marginBottom: "24px",
+  fontFamily: "Inter",
+  fontWeight: 400,
+  fontSize: "16px",
+  lineHeight: "22px",
+  color: "#86E2F6",
+});
+
+const StyledButton = styled(Button)({
+  margin: "0 auto",
+  padding: "10px 20px",
+  color: "#004A80 !important",
+  background: "#FFFFFF !important",
+  width: "176px",
+  fontSize: "16px",
+  fontWeight: 700,
+});
 
 const Page404: FC = () => {
   usePageTitle("Page Not Found");
 
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page not found</p>
-    </div>
+    <StyledContainer>
+      <StyledContent>
+        <StyledImage src={status404} alt="404 icon" />
+        <StyledHeader variant="h1">Page not found.</StyledHeader>
+        <StyledBody variant="body1">Looks like you got a little turned around.</StyledBody>
+        <StyledButton variant="contained" color="primary" onClick={handleClick}>
+          Return Home
+        </StyledButton>
+      </StyledContent>
+    </StyledContainer>
   );
 };
 
