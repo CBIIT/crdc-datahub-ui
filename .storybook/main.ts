3c54d78d306c133ed63b71a4f7e180c212b15711
Alec M <alec.mattu@nih.gov>
2024-06-03 10:08:02 -0400

    deps: Update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(ts|tsx)"],
+
   addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
@@ -10,14 +11,20 @@ const config: StorybookConfig = {
     "@storybook/addon-jest",
     "@storybook/addon-a11y",
     "storybook-addon-apollo-client",
+    "@chromatic-com/storybook"
   ],
+
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
+  docs: {},
+
   staticDirs: ["../public"],
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
