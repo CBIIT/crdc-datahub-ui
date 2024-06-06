26bf89c32311b8f0c3e90c52694255488bc70554
Alejandro-Vega <vegaalejandro15@gmail.com>
2024-06-06 11:26:30 -0400

    Memoize OrganizationProvider and added filter inactive orgs

--- src/content/dataSubmissions/Controller.tsx
+++ src/content/dataSubmissions/Controller.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { memo } from "react";
 import { useParams } from "react-router-dom";
 import DataSubmission from "./DataSubmission";
 import ListView from "./DataSubmissionsListView";
 import { OrganizationProvider } from "../../components/Contexts/OrganizationListContext";
+
+/**
+ * A memoized version of OrganizationProvider
+ *
+ * @see OrganizationProvider
+ */
+const MemorizedProvider = memo(OrganizationProvider);
 
 /**
  * Render the correct view based on the URL
@@ -18,9 +25,9 @@ const DataSubmissionController = () => {
   }
 
   return (
-    <OrganizationProvider preload>
+    <MemorizedProvider preload filterInactive>
       <ListView />
-    </OrganizationProvider>
+    </MemorizedProvider>
   );
 };
 
