ce13e54fc6111cd1f558c0c59e92e7d9c7bf0d9e
Alec M <alec.mattu@nih.gov>
2024-09-24 09:32:57 -0400

    revert: Unnecessary console spy

--- src/components/Footer/index.test.tsx
+++ src/components/Footer/index.test.tsx
@@ -4,14 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import Footer from "./index";
 
 describe("Footer", () => {
-  beforeEach(() => {
-    jest.spyOn(console, "error").mockImplementation();
-  });
-
-  afterAll(() => {
-    jest.restoreAllMocks();
-  });
-
   it("should not have any accessibility violations", async () => {
     const { container } = render(<Footer />, { wrapper: (p) => <BrowserRouter {...p} /> });
     const results = await axe(container);
