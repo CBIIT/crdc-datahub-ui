5f9df84a744511559ac090a02e1ed31a5951b07c
Alejandro-Vega <vegaalejandro15@gmail.com>
2024-09-27 11:04:09 -0400

    Update study queries and types

--- src/graphql/listApprovedStudies.ts
+++ src/graphql/listApprovedStudies.ts
@@ -1,16 +1,54 @@
 import gql from "graphql-tag";
 
 export const query = gql`
-  query listApprovedStudies {
-    listApprovedStudies {
-      _id
-      studyName
-      studyAbbreviation
-      dbGaPID
+  query listApprovedStudies(
+    $first: Int
+    $offset: Int
+    $orderBy: String
+    $sortDirection: String
+    $dbGaPID: String
+    $controlledAccess: String
+    $study: String
+  ) {
+    listApprovedStudies(
+      first: $first
+      offset: $offset
+      orderBy: $orderBy
+      sortDirection: $sortDirection
+      dbGaPID: $dbGaPID
+      controlledAccess: $controlledAccess
+      study: $study
+    ) {
+      total
+      studies {
+        _id
+        studyName
+        studyAbbreviation
+        dbGaPID
+        controlledAccess
+        openAccess
+        PI
+        ORCID
+        createdAt
+      }
     }
   }
 `;
 
+export type Input = {
+  first?: number;
+  offset?: number;
+  orderBy?: string;
+  sortDirection?: Order;
+  dbGaPID?: string;
+  controlledAccess?: AccessType;
+  openAccess?: boolean;
+  study?: string;
+};
+
 export type Response = {
-  listApprovedStudies: ApprovedStudy[];
+  listApprovedStudies: {
+    total: number;
+    studies: ApprovedStudy[];
+  };
 };
