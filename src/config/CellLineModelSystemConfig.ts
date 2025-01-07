109467426ff10d722f9d3a12a4ec9201f60d49f0
Alec M <alec.mattu@nih.gov>
2025-01-07 13:36:26 -0500

    CRDCDH-1861 Remove tooltips

--- src/config/CellLineModelSystemConfig.ts
+++ src/config/CellLineModelSystemConfig.ts
@@ -6,13 +6,11 @@ const options: FormGroupCheckboxOption[] = [
     label: "Cell lines",
     value: "Cell lines",
     name: "cellLines",
-    tooltipText: "TODO: Add tooltip text from PM",
   },
   {
     label: "Model systems",
     value: "Model systems",
     name: "modelSystems",
-    tooltipText: "TODO: Add tooltip text from PM",
   },
 ];
 
