2323b25e50256a93e35dfbe81941d1d689c972d1
Alec M <alec.mattu@nih.gov>
2025-03-10 14:48:21 -0400

    Add stories for Submission Request review comments dialogs

--- src/components/CancelApplicationButton/index.stories.tsx
+++ src/components/CancelApplicationButton/index.stories.tsx
@@ -30,9 +30,10 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 /**
- * The button that allows the user to cancel an application.
+ * The button that allows the user to restore an application.
  */
-export const Restore: Story = {
+export const RestoreCanceled: Story = {
+  name: "Restore (From Cancelled)",
   args: {
     application: {
       _id: "mock-id",
@@ -51,6 +52,46 @@ export const Restore: Story = {
       controlledAccess: false,
       openAccess: false,
       studyAbbreviation: "Mock Study that is canceled",
+      conditional: false,
+      pendingConditions: [],
+      programName: "",
+      programAbbreviation: "",
+      programDescription: "",
+      version: "",
+    },
+  },
+  argTypes: {
+    application: {
+      control: {
+        disable: true,
+      },
+    },
+  },
+};
+
+/**
+ * The button that allows the user to restore an application.
+ */
+export const RestoreDeleted: Story = {
+  name: "Restore (From Deleted)",
+  args: {
+    application: {
+      _id: "mock-id",
+      status: "Deleted",
+      createdAt: "",
+      updatedAt: "",
+      submittedDate: "",
+      history: [],
+      ORCID: "",
+      applicant: {
+        applicantID: "applicant-123",
+        applicantName: "",
+        applicantEmail: "",
+      },
+      PI: "",
+      controlledAccess: false,
+      openAccess: false,
+      studyAbbreviation: "Mock Study that is deleted",
       conditional: false,
       pendingConditions: [],
       programName: "",
