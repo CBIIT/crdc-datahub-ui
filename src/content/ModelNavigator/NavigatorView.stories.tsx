3e035d5e4fae80307b5a5452b00ff2799ec6c02f
Alec M <alec.mattu@nih.gov>
2025-03-24 09:54:16 -0400

    Dynamically set dev tier variable

--- src/content/ModelNavigator/NavigatorView.stories.tsx
+++ src/content/ModelNavigator/NavigatorView.stories.tsx
@@ -3,10 +3,12 @@ import NavigatorView from "./NavigatorView";
 import { DataCommonProvider } from "../../components/Contexts/DataCommonContext";
 import ErrorBoundary from "../../components/ErrorBoundary";
 import { DataCommons } from "../../config/DataCommons";
+import env from "../../env";
 
 type CustomStoryProps = React.ComponentProps<typeof NavigatorView> & {
   model: string;
   version?: string;
+  tier?: AppEnv["REACT_APP_DEV_TIER"];
 };
 
 const meta: Meta<CustomStoryProps> = {
@@ -22,18 +24,34 @@ const meta: Meta<CustomStoryProps> = {
     version: {
       control: "text",
       description: "The version of the model to display",
-      defaultValue: "latest",
+    },
+    tier: {
+      control: "select",
+      description: "The deployment tier",
+      options: ["dev", "dev2", "qa", "qa2", "stage", "prod"],
     },
   },
   parameters: { layout: "fullscreen" },
   decorators: [
-    (Story, context) => (
-      <ErrorBoundary key={`${context.args.model}-${context.args.version}`}>
-        <DataCommonProvider DataCommon={context.args.model}>
-          <Story />
-        </DataCommonProvider>
-      </ErrorBoundary>
-    ),
+    (Story, context) => {
+      try {
+        // Remove the model manifest from session storage
+        sessionStorage.removeItem("manifest");
+
+        // Set the dev tier to fetch the correct manifest from
+        env.REACT_APP_DEV_TIER = context.args.tier;
+      } catch (e) {
+        /* empty */
+      }
+
+      return (
+        <ErrorBoundary key={`${context.args.tier}-${context.args.model}-${context.args.version}`}>
+          <DataCommonProvider DataCommon={context.args.model}>
+            <Story />
+          </DataCommonProvider>
+        </ErrorBoundary>
+      );
+    },
   ],
 } satisfies Meta<CustomStoryProps>;
 
@@ -44,5 +62,6 @@ export const ModelNavigator: Story = {
   args: {
     model: "CDS",
     version: "latest",
+    tier: "dev",
   },
 };
