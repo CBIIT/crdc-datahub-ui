4c45830ed8e940617411c0edcbd186028de4e274
Alec M <alec.mattu@nih.gov>
2025-03-28 15:26:18 -0400

    Replace dot import

--- src/components/ReviewCommentsDialog/index.test.tsx
+++ src/components/ReviewCommentsDialog/index.test.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider, rgbToHex } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { render, fireEvent, waitFor } from "@testing-library/react";
 import { axe } from "jest-axe";
-import ReviewCommentsDialog from ".";
+import ReviewCommentsDialog from "./index";
 import theme from "../../theme";
 
 type Props<T, H> = {
