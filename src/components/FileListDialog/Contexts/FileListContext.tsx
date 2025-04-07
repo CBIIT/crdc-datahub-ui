631702e6201d5bfb76f934ca81bf81b0c524e886
Alec M <alec.mattu@nih.gov>
2025-04-07 10:35:02 -0400

    Remove default context value Increases coverage and isn't really necessary

--- src/components/FileListDialog/Contexts/FileListContext.tsx
+++ src/components/FileListDialog/Contexts/FileListContext.tsx
@@ -4,25 +4,21 @@ export type FileListContextState = {
   /**
    * Boolean flag to indicate if actions should be disabled.
    */
-  disabled: boolean;
+  disabled?: boolean;
   /**
    * Boolean flag to indicate if actions should be hidden.
    */
-  hidden: boolean;
+  hidden?: boolean;
   /**
    * Callback handler when the Download button is clicked.
    */
-  handleDownloadClick: (fileName: string) => Promise<void>;
+  handleDownloadClick?: (fileName: string) => Promise<void>;
 };
 
 /**
  * Context for the FileListDialog to provide a callback handler for the Download button.
  * This allows the parent component to define what happens when a file is downloaded.
  */
-const FileListContext = React.createContext<FileListContextState>({
-  disabled: false,
-  hidden: false,
-  handleDownloadClick: async () => {},
-});
+const FileListContext = React.createContext<FileListContextState>({});
 
 export default FileListContext;
