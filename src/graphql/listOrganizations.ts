ea56d7232cba3a18bd05fedda2c54f7094e54d88
Alec M <alec.mattu@nih.gov>
2025-04-24 10:45:17 -0400

    CRDCDH-2598 Compact Programs Table, fix API integration

--- src/graphql/listOrganizations.ts
+++ src/graphql/listOrganizations.ts
@@ -1,38 +1,65 @@
+import { TypedDocumentNode } from "@apollo/client";
 import gql from "graphql-tag";
 
-export const query = gql`
-  query listOrganizations {
-    listOrganizations {
-      _id
-      name
-      abbreviation
-      description
-      status
-      conciergeName
-      studies {
-        studyName
-        studyAbbreviation
+export const query: TypedDocumentNode<Response, Input> = gql`
+  query listPrograms(
+    $status: String!
+    $first: Int
+    $offset: Int
+    $orderBy: String
+    $sortDirection: String
+  ) {
+    listPrograms(
+      status: $status
+      first: $first
+      offset: $offset
+      orderBy: $orderBy
+      sortDirection: $sortDirection
+    ) {
+      total
+      programs {
+        _id
+        name
+        abbreviation
+        description
+        status
+        conciergeName
+        studies {
+          studyName
+          studyAbbreviation
+        }
+        createdAt
+        updateAt
       }
-      createdAt
-      updateAt
     }
   }
 `;
 
+export type Input = {
+  status: "All" | Organization["status"];
+  first?: number;
+  offset?: number;
+  orderBy?: keyof Organization;
+  sortDirection?: "asc" | "desc";
+};
+
 export type Response = {
-  listOrganizations: Array<
-    Pick<
-      Organization,
-      | "_id"
-      | "name"
-      | "abbreviation"
-      | "description"
-      | "status"
-      | "conciergeName"
-      | "createdAt"
-      | "updateAt"
-    > & {
-      studies: Pick<Organization["studies"][number], "studyName" | "studyAbbreviation">[];
-    }
-  >;
+  listPrograms: {
+    total: number;
+    programs: Array<
+      Pick<
+        Organization,
+        | "_id"
+        | "name"
+        | "abbreviation"
+        | "description"
+        | "status"
+        | "conciergeName"
+        | "createdAt"
+        | "updateAt"
+      > & {
+        studies: Pick<Organization["studies"][number], "studyName" | "studyAbbreviation">[];
+      }
+    >;
+  };
 };
