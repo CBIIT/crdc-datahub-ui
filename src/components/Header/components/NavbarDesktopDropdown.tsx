ad302fa63ced99de395571705b25b9db6d9af2b4
Alejandro-Vega <vegaalejandro15@gmail.com>
2025-05-06 13:55:05 -0400

    Bring over initial styling changes

--- src/components/Header/components/NavbarDesktopDropdown.tsx
+++ src/components/Header/components/NavbarDesktopDropdown.tsx
@@ -1,49 +1,68 @@
 import { Button, styled } from "@mui/material";
 import { useMemo, useRef } from "react";
+import Grid from "@mui/material/Unstable_Grid2";
 import { generatePath, Link } from "react-router-dom";
 import { Status, useAuthContext } from "../../Contexts/AuthContext";
 import { HeaderSubLinks } from "../../../config/HeaderConfig";
 import SuspenseLoader from "../../SuspenseLoader";
 
 const Dropdown = styled("div")({
-  top: "60.5px",
   left: 0,
+  right: 0,
   width: "100%",
   background: "#1F4671",
   zIndex: 1100,
+  display: "block",
   position: "absolute",
+  paddingTop: "35px",
+  paddingBottom: "12px",
 });
 
-const NameDropdownContainer = styled("div")({
+const StyledGridContainer = styled(Grid)({
   margin: "0 auto",
   textAlign: "left",
   position: "relative",
   maxWidth: "1400px",
+  padding: "0 16px",
   "& .dropdownList": {
-    background: "#1F4671",
-    display: "inline-flex",
-    gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
-    padding: "32px 32px 0 32px",
+    padding: 0,
+    marginTop: 0,
+    marginBottom: "45px",
+    listStyle: "none",
+  },
+  "& .gridItem": {
+    paddingLeft: "16px",
+    paddingRight: "16px",
+  },
+  "& .dropdownListItem": {
+    padding: 0,
+    margin: 0,
+    lineHeight: "22px",
+    height: "22px",
   },
   "& .dropdownItem": {
-    padding: "0 10px 52px 10px",
+    padding: "0",
     textAlign: "left",
     fontFamily: "'Poppins', sans-serif",
     fontStyle: "normal",
     fontWeight: 600,
     fontSize: "20px",
-    lineHeight: "110%",
+    lineHeight: "22px",
     color: "#FFFFFF",
     textDecoration: "none",
     cursor: "pointer",
+    whiteSpace: "pre-line",
     "&:hover": {
       textDecoration: "underline",
     },
   },
+  "& a.dropdownItem": {
+    display: "inline-block",
+    padding: "0",
+  },
   "& .dropdownItemButton": {
     textTransform: "none",
-    paddingLeft: "20px",
-    paddingRight: "20px",
+    paddingLeft: 0,
     "&:hover": {
       background: "transparent",
     },
@@ -78,59 +97,67 @@ const NavbarDesktopDropdown = ({ clickedTitle, onTitleClick, onItemClick }: Prop
 
   return (
     <Dropdown ref={dropdownSelection} className={clickedTitle === "" ? "invisible" : ""}>
-      <NameDropdownContainer>
-        <div className="dropdownList">
-          {clickedTitle !== ""
-            ? HeaderSubLinks?.[clickedTitle]?.map((dropItem) => {
-                if (
-                  dropItem?.permissions?.length > 0 &&
-                  !dropItem?.permissions?.every(
-                    (permission: AuthPermissions) => user?.permissions?.includes(permission)
-                  )
-                ) {
-                  return null;
-                }
+      <StyledGridContainer container>
+        {clickedTitle !== ""
+          ? HeaderSubLinks?.[clickedTitle]?.map((dropItem) => {
+              if (
+                dropItem?.permissions?.length > 0 &&
+                !dropItem?.permissions?.every(
+                  (permission: AuthPermissions) => user?.permissions?.includes(permission)
+                )
+              ) {
+                return null;
+              }
 
-                if (dropItem.link) {
-                  const to =
-                    dropItem.link.includes(":") && paramGetters?.[dropItem.name]
-                      ? generatePath(dropItem.link, paramGetters[dropItem.name]())
-                      : dropItem.link;
-
-                  return (
-                    <span className="dropdownItem" key={dropItem.id}>
-                      <Link
-                        target={
-                          dropItem.link.startsWith("https://") || dropItem.link.endsWith(".pdf")
-                            ? "_blank"
-                            : "_self"
-                        }
-                        id={dropItem.id}
-                        to={to}
-                        className="dropdownItem"
-                        onClick={() => onTitleClick("")}
-                      >
-                        {dropItem.name}
-                        {dropItem.text && <div className="dropdownItemText">{dropItem.text}</div>}
-                      </Link>
-                    </span>
-                  );
-                }
+              if (dropItem.link) {
+                const to =
+                  dropItem.link.includes(":") && paramGetters?.[dropItem.name]
+                    ? generatePath(dropItem.link, paramGetters[dropItem.name]())
+                    : dropItem.link;
 
                 return (
-                  <Button
-                    id={dropItem.id}
-                    key={dropItem.id}
-                    className="dropdownItem dropdownItemButton"
-                    onClick={() => onItemClick?.(dropItem.name)}
-                  >
-                    {dropItem.name}
-                  </Button>
+                  <Grid xs={3} key={dropItem.id} className="gridItem">
+                    <ul className="dropdownList">
+                      <li className="dropdownListItem">
+                        <Link
+                          target={
+                            dropItem.link.startsWith("https://") || dropItem.link.endsWith(".pdf")
+                              ? "_blank"
+                              : "_self"
+                          }
+                          id={dropItem.id}
+                          to={to}
+                          className="dropdownItem"
+                          onClick={() => onTitleClick("")}
+                        >
+                          {dropItem.name}
+                          {dropItem.text && <div className="dropdownItemText">{dropItem.text}</div>}
+                        </Link>
+                      </li>
+                    </ul>
+                  </Grid>
                 );
-              })
-            : null}
-        </div>
-      </NameDropdownContainer>
+              }
+
+              return (
+                <Grid xs={3} key={dropItem.id} className="gridItem">
+                  <ul className="dropdownList">
+                    <li className="dropdownListItem">
+                      <Button
+                        id={dropItem.id}
+                        key={dropItem.id}
+                        className="dropdownItem dropdownItemButton"
+                        onClick={() => onItemClick?.(dropItem.name)}
+                      >
+                        {dropItem.name}
+                      </Button>
+                    </li>
+                  </ul>
+                </Grid>
+              );
+            })
+          : null}
+      </StyledGridContainer>
     </Dropdown>
   );
 };
