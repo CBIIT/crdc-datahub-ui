57c4da69dbcb9a2ec71a853704dea7e13f4624e2
Alejandro-Vega <vegaalejandro15@gmail.com>
2025-05-23 13:46:54 -0400

    fix: lodash global not initialized, causing DMN to crash

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ApolloProvider } from "@apollo/client";
+import _ from "lodash";
 import App from "./App";
 import client from "./client";
 import { AuthProvider } from "./components/Contexts/AuthContext";
 import { AnalyticsProvider } from "./components/Contexts/AnalyticsContext";
 import env from "./env";
+
+// Required for DMN to work properly
+globalThis._ = _;
 
 if (env.VITE_DEV_TIER === "dev" || env.VITE_DEV_TIER === "dev2") {
   import("@axe-core/react").then(({ default: axe }) => axe(React, ReactDOM, 1500));
