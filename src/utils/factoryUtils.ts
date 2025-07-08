46e0b2d2353fb41d4a2307e0bf40f78846a9d352
Alejandro-Vega <vegaalejandro15@gmail.com>
2025-07-08 14:09:29 -0400

    refactor: general cleanup of the atttach helper functions

--- src/utils/factoryUtils.ts
+++ src/utils/factoryUtils.ts
@@ -3,36 +3,39 @@ import { cloneDeep } from "@apollo/client/utilities/common/cloneDeep";
 /**
  * Attaches a __typename property to a single object
  *
- * @param {T} data The object to attach the __typename to
+ * @param {T} data - The object to attach the __typename to
  * @returns {BuildableSingle<T>} A buildable object with a non-enumerable withTypename method
  */
 export const attachSingleWithTypename = <T>(data: T): BuildableSingle<T> => {
-  const item = cloneDeep(data);
-  const buildable = item as BuildableSingle<T>;
+  const buildable = cloneDeep(data) as BuildableSingle<T>;
+
   Object.defineProperty(buildable, "withTypename", {
     value: <K extends string>(typename: K) =>
-      ({ ...item, __typename: typename }) as T & { __typename: K },
+      ({ ...buildable, __typename: typename }) as T & { __typename: K },
     writable: false,
     enumerable: false,
     configurable: false,
   });
+
   return buildable;
 };
 
 /**
  * Attaches a __typename property to an array of objects
  *
- * @param {T[]} items The array of objects to attach the __typename to
+ * @param {T[]} data - The array of objects to attach the __typename to
  * @returns {BuildableArray<T>} A buildable array with a non-enumerable withTypename method
  */
-export const attachMultipleWithTypename = <T>(items: T[]): BuildableArray<T> => {
-  const arr = cloneDeep(items) as BuildableArray<T>;
-  Object.defineProperty(arr, "withTypename", {
+export const attachMultipleWithTypename = <T>(data: T[]): BuildableArray<T> => {
+  const buildable = cloneDeep(data) as BuildableArray<T>;
+
+  Object.defineProperty(buildable, "withTypename", {
     value: <K extends string>(typename: K) =>
-      arr.map((item) => ({ ...item, __typename: typename }) as T & { __typename: K }),
+      buildable.map((item) => ({ ...item, __typename: typename }) as T & { __typename: K }),
     writable: false,
     enumerable: false,
     configurable: false,
   });
-  return arr;
+
+  return buildable;
 };
