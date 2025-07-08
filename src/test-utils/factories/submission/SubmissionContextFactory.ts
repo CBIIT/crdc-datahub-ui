959f7e487ecb94c584cc75e0dc169db56d5932f4
Alejandro-Vega <vegaalejandro15@gmail.com>
2025-07-08 17:23:58 -0400

    add additional submission factories

---
+++ src/test-utils/factories/submission/SubmissionContextFactory.ts
@@ -0,0 +1,34 @@
+import { SubmissionCtxState, SubmissionCtxStatus } from "@/components/Contexts/SubmissionContext";
+
+import { Factory } from "../Factory";
+
+import { submissionAttributesFactory } from "./SubmissionAttributes";
+import { submissionFactory } from "./SubmissionFactory";
+
+/**
+ * Base SubmissionCtxState object
+ */
+export const baseSubmissionCtxState: SubmissionCtxState = {
+  status: SubmissionCtxStatus.LOADING,
+  data: {
+    getSubmission: submissionFactory.build(),
+    getSubmissionAttributes: {
+      submissionAttributes: submissionAttributesFactory
+        .pick(["isBatchUploading", "hasOrphanError"])
+        .build(),
+    },
+  },
+  error: null,
+  refetch: null,
+  startPolling: null,
+  stopPolling: null,
+  updateQuery: null,
+};
+
+/**
+ * SubmissionCtxState factory for creating SubmissionCtxState instances
+ */
+export const submissionCtxStateFactory = new Factory<SubmissionCtxState>((overrides) => ({
+  ...baseSubmissionCtxState,
+  ...overrides,
+}));
