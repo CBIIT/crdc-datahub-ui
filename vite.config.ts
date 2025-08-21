12fa718e1080067d46241ca7df3cce44b6ddd01d
Alec M. <alec.mattu@nih.gov>
2025-08-21 16:13:55 -0400

    Add Relative CI script

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import webpackStats from "rollup-plugin-webpack-stats";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import viteTsConfigPaths from "vite-tsconfig-paths";
@@ -27,6 +28,7 @@ export default defineConfig({
       svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
     }),
     viteTsConfigPaths(),
+    webpackStats(),
   ],
   server: {
     open: true,
